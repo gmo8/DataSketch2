e11589eedd363cd2768326edab3a9c0edc3be860
Gianna Ossello <gianna@Giannas-MacBook-Air.local>
2018-03-20 09:46:30 -0400

    firts commit

--- smokysketch5.js
+++ smokysketch5.js
@@ -46,34 +46,3 @@ function loadData(){
   }
 }
 
-class Species {
-    constructor(photo, x, y, diameter, s) {
-      this.x = Number(x);
-      this.y = Number(y);
-      this.diameter = Number(diameter);
-      this.name = s;
-      this.over = false;
-      this.photo = photo; 
-    }
-      rollover(px, py) {
-        var d = dist(px, py, this.x, this.y);
-        if (d < this.diameter) {
-          this.over = true;
-        } else {
-          this.over = false;
-        }
-    }
-
-display() {
-    stroke(5);
-    strokeWeight(5);
-    image(this.photo, this.x, this.y, this.diameter, this.diameter);
-    if (this.over) {
-      textAlign(CENTER);
-      noStroke();
-      fill(0);
-      text(this.name, this.x, this.y + this.diameter/2 + 20);
-      
-    }
-  }
-}
